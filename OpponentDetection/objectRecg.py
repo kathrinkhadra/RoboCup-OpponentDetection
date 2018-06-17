f44fa155ef18ac5004c42b2b46985d4510129187
ROBOCUP17 <ga96tum@mytum.de>
2018-06-17 15:40:26 +0200

    Updating Opponent Detection

--- OpponentDetection/objectRecg.py
+++ OpponentDetection/objectRecg.py
@@ -17,179 +17,182 @@ from os import listdir
 from os.path import isfile, join
 
 def main():
-	#Takes all of the images on the file specified folder
-	mypath = "images/"
-	fileNames = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-	imd = np.empty(len(fileNames), dtype=object)
-	for n in range(0, len(fileNames)):
-		imd[n] = cv2.imread( join(mypath,fileNames[n]) )
+    #Takes all of the images on the file specified folder
+    mypath = "images/test/"
+    fileNames = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+    imd = np.empty(len(fileNames), dtype=object)
+    for n in range(0, len(fileNames)):
+            if fileNames[n]=="template.png":
+                imq=cv2.imread(join(mypath,fileNames[n]))
+    for n in range(0,len(fileNames)):
+                imd[n] = cv2.imread(join(mypath,fileNames[n]))
 
-	print("Starting image matching process.")
+    print("Starting image matching process.")
 
-	#Goes through each and every single image setting the first one as query, and all as database.
-	for n in range(0, len(imd)):
-		imq = imd[n].copy()
+    #Goes through each and every single image setting the first one as query, and all as database.
+#    for n in range(0, len(imd)):
+#        imq = imd[n].copy()
+    
+    templateTopFour, templateMatchingScores = templateMatching(imd, imq, n)
+    topFourImages(templateTopFour, fileNames, "template.png", "Template Matching")
+    imageScorePlacing(templateMatchingScores, fileNames, "template.png", "Template Matching")
 
-		templateTopFour, templateMatchingScores = templateMatching(imd, imq, n)
-		#topFourImages(templateTopFour, fileNames, n, "Template Matching")
-		#imageScorePlacing(templateMatchingScores, fileNames, n, "Template Matching")
+    histogramTopFour, histogramMatchingScores = histogramMatching(imd, imq, n)
+    topFourImages(histogramTopFour, fileNames, "template.png", "Histogram Matching")
+    imageScorePlacing(histogramMatchingScores, fileNames, "template.png", "Histogram Matching")
 
-		histogramTopFour, histogramMatchingScores = histogramMatching(imd, imq, n)
-		#topFourImages(histogramTopFour, fileNames, n, "Histogram Matching")
-		#imageScorePlacing(histogramMatchingScores, fileNames, n, "Histogram Matching")
+    siftTopFour, siftMatchingScores = SIFT(imd, imq, n)
+    topFourImages(siftTopFour, fileNames, "template.png", "SIFT")
+    imageScorePlacing(siftMatchingScores, fileNames, "template.png", "SIFT")
 
-		siftTopFour, siftMatchingScores = SIFT(imd, imq, n)
-		#topFourImages(siftTopFour, fileNames, n, "SIFT")
-		#imageScorePlacing(siftMatchingScores, fileNames, n, "SIFT")
+    print 
 
-		print (siftTopFour)
-
-	print("Done.")
+    print("Done.")
 
 
 
 def templateMatching(imd, imq, imageNumber):
-	methods = ['cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF_NORMED'] #[3, 1]
+    methods = ['cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF_NORMED'] #[3, 1]
 
-	#dict to hold template matching results.
-	topFour = {}
-	totalScores = {}
+    #dict to hold template matching results.
+    topFour = {}
+    totalScores = {}
 
 
-	for n in range(0, len(imd)):
-		im = imd[n].copy()
-		resArr = []
+    for n in range(0, len(imd)):
+        im = imd[n].copy()
+        resArr = []
 
-		for meth in methods:
-			method = eval(meth)
+        for meth in methods:
+            method = eval(meth)
 
-			# Apply template Matching
-			res = cv2.matchTemplate(im,imq,method)
-			min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            # Apply template Matching
+            res = cv2.matchTemplate(im,imq,method)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-			#Save each template score into an array. We take the inverse on TM_SQDIFF_NORMED
-			if eval(meth) == 1:
-				res = 1 - res
-				resArr.append(res)
-			else:
-				resArr.append(res)
-			
-		pass
-		#add each score of the array and take average
-		score = np.sum(resArr) / len(resArr)
-		totalScores[n] = score
+            #Save each template score into an array. We take the inverse on TM_SQDIFF_NORMED
+            if eval(meth) == 1:
+                res = 1 - res
+                resArr.append(res)
+            else:
+                resArr.append(res)
+            
+        pass
+        #add each score of the array and take average
+        score = np.sum(resArr) / len(resArr)
+        totalScores[n] = score
 
-		#save top 4 scores
-		if len(topFour) < 4:
-			topFour[n] = score
-		else:
-			topFour = topFourDictionary(topFour, n, score)
+        #save top 4 scores
+        if len(topFour) < 4:
+            topFour[n] = score
+        else:
+            topFour = topFourDictionary(topFour, n, score)
 
-	return topFour, totalScores
+    return topFour, totalScores
 
 
 def histogramMatching(imd, imq, imageNumber):
-	#dict to hold final histogram matching results.
-	topFour = {}
-	totalScores = {}
+    #dict to hold final histogram matching results.
+    topFour = {}
+    totalScores = {}
 
-	#calculate histogram of query image.
-	histq = cv2.calcHist([imq], [0], None, [256], [0, 256])
-	
-	#calculate histogram of each database image.
-	histd = np.empty(len(imd), dtype=object)
-	for n in range(0, len(imd)):
-		histd[n] = cv2.calcHist([imd[n]], [0], None, [256], [0, 256])
+    #calculate histogram of query image.
+    histq = cv2.calcHist([imq], [0], None, [256], [0, 256])
+    
+    #calculate histogram of each database image.
+    histd = np.empty(len(imd), dtype=object)
+    for n in range(0, len(imd)):
+        histd[n] = cv2.calcHist([imd[n]], [0], None, [256], [0, 256])
 
-	#go through all the methods on each image and take average.
-	for n in range(0, len(imd)):
-		resArr = []
-		resArr.append(cv2.compareHist(histq, histd[n], cv2.HISTCMP_CORREL))
-		resArr.append(1 - cv2.compareHist(histq, histd[n], cv2.HISTCMP_BHATTACHARYYA))
+    #go through all the methods on each image and take average.
+    for n in range(0, len(imd)):
+        resArr = []
+        resArr.append(cv2.compareHist(histq, histd[n], cv2.HISTCMP_CORREL))
+        resArr.append(1 - cv2.compareHist(histq, histd[n], cv2.HISTCMP_BHATTACHARYYA))
 
-		score = np.sum(resArr) / len(resArr)
-		totalScores[n] = score
+        score = np.sum(resArr) / len(resArr)
+        totalScores[n] = score
 
-		#save top 4 scores
-		if len(topFour) < 4:
-			topFour[n] = score
-		else:
-			topFour = topFourDictionary(topFour, n, score)
+        #save top 4 scores
+        if len(topFour) < 4:
+            topFour[n] = score
+        else:
+            topFour = topFourDictionary(topFour, n, score)
 
-	return topFour, totalScores
+    return topFour, totalScores
 
 
 
 def SIFT(imd, imq, imageNumber):
-	#dict to hold final SIFT matching results.
-	topFour = {}
-	totalScores = {}
-	
-	for z in range(0, len(imd)):
-		img1 = imq.copy()
-		img2 = imd[z].copy()
+    #dict to hold final SIFT matching results.
+    topFour = {}
+    totalScores = {}
+    
+    for z in range(0, len(imd)):
+        img1 = imq.copy()
+        img2 = imd[z].copy()
 
-		# Initiate SIFT detector
-		sift = cv2.xfeatures2d.SIFT_create()
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
 
-		# find the keypoints and descriptors with SIFT
-		kp1, des1 = sift.detectAndCompute(img1,None)
-		kp2, des2 = sift.detectAndCompute(img2,None)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
 
-		FLANN_INDEX_KDTREE = 0
-		index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-		search_params = dict(checks = 50)
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
 
-		flann = cv2.FlannBasedMatcher(index_params, search_params)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-		matches = flann.knnMatch(des1,des2,k=2)
+        matches = flann.knnMatch(des1,des2,k=2)
 
-		# store all the good matches as per Lowe's ratio test.
-		good = []
-		for m,n in matches:
-			if m.distance < 0.7*n.distance:
-				good.append(m)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
 
-		score = len(good)
-		totalScores[z] = score
+        score = len(good)
+        totalScores[z] = score
 
-		#save top 4 scores
-		if len(topFour) < 4:
-			topFour[z] = score
-		else:
-			topFour = topFourDictionary(topFour, z, score)
+        #save top 4 scores
+        if len(topFour) < 4:
+            topFour[z] = score
+        else:
+            topFour = topFourDictionary(topFour, z, score)
 
-	return topFour, totalScores
+    return topFour, totalScores
 
 #We save the location in the fileNames array (n), which holds the image name, and its score (score).
 #If the new image score is lower we swap.
 def topFourDictionary(topFour, n, score):
-	for j, k in topFour.items():
-				if k < score:
-					del topFour[j]
-					topFour[n] = score
-					return topFour
+    for j, k in topFour.items():
+                if k < score:
+                    del topFour[j]
+                    topFour[n] = score
+                    return topFour
 
-	return topFour
+    return topFour
 
 def topFourImages(topFour, fileNames, image, method):
-	print(fileNames[image] + " top matches in: " + method)
+    print(image + " top matches in: " + method)
 
-	for n, m in topFour.items():
-		print fileNames[n]
-	print
+    for n, m in topFour.items():
+        print fileNames[n]
+    print
 
 def imageScorePlacing(allScores, fileNames, image, method):
-	print(fileNames[image] + " all scores in: " + method)
-	count = len(allScores)
+    print(image + " all scores in: " + method)
+    count = len(allScores)
 
-	for key, value in sorted(allScores.iteritems(), key = lambda (k, v): (v, k)):
-		print count, fileNames[key]
-		count = count - 1
+    for key, value in sorted(allScores.iteritems(), key = lambda (k, v): (v, k)):
+        print count, fileNames[key]
+        count = count - 1
 
-	print
+    print
 
 
 
 if __name__ == "__main__":
-	main()
+    main()
